7f53ce5952b4d78c7cbf4b45c61f8f7506f50ca1
Stesha McKindle <steshamckindle@gmail.com>
2020-08-05 21:20:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+adjectives = ['attractive', 'agreeable', 'angry', 'big', 'flabby', 'kind', 'lazy', 'microscopic', 'skinny', 'witty', 'scary', 'tall', 'jolly', 'zealous', 'gentle']
+
+nouns = ['people', 'history', 'way', 'art', 'world', 'youth', 'uncle', 'trainer', 'throat', 'tale', 'love', 'internet', 'television', 'science', 'library']
+
+
+
+print("Welcome to the Password Picker!")
