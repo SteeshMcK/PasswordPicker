ed487aaec81aa692582f92acb471a5020338c1b0
Stesha McKindle <steshamckindle@gmail.com>
2020-08-07 20:25:41 +0000

    Add array of colors to enable a greater number of unique passwords

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import string
 
 adjectives = ['attractive', 'agreeable', 'angry', 'big', 'flabby', 'kind', 'lazy', 'microscopic', 'skinny', 'witty', 'scary', 'tall', 'jolly', 'zealous', 'gentle']
 
+colors = ['Cotton', 'Sand', 'Banana', 'Cantaloupe', 'Garnet', 'Rouge', 'Violet', 'Cerulean', 'Pine', 'Hickory', 'Dove', 'Fog', 'Flint', 'Raven', 'Spider']
+
 nouns = ['People', 'History', 'Way', 'Art', 'World', 'Youth', 'Uncle', 'Trainer', 'Throat', 'Tale', 'Love', 'Internet', 'Television', 'Science', 'Library']
 
 print("Welcome to the Password Picker!")
@@ -13,6 +15,7 @@ while True:
   for num in range(howMany):
     #Use choice() from the random module to make a random choice from the two arrays
     adjective = random.choice(adjectives)
+    color = random.choice(colors)
     noun = random.choice(nouns)
 
     #Use randrange() from random module to choose a random number within the range given
@@ -22,7 +25,7 @@ while True:
     #It holds a string of punctuation characters
     special_char = random.choice(string.punctuation)
 
-    password = adjective + noun + str(number) + special_char
+    password = adjective + color + noun + str(number) + special_char
     print("Your new password is: "  + password)
 
   response = input("Would you me to create more passwords? Type y or n: ")
